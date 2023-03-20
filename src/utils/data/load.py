ef432d1af79a4f9b31b8a47a46c972a31e2de823
Gabriel Lara <gtchaves1@ihm.com.br>
2023-03-20 14:52:21 -0300

    Adds seasonal signal loading function

---
+++ src/utils/data/load.py
@@ -0,0 +1,41 @@
+"""
+Functions for raw data loading.
+"""
+from typing import Optional, Union
+import pickle
+import numpy as np
+import pandas as pd
+from scipy.signal import decimate
+from scipy.ndimage import uniform_filter1d
+
+
+def get_seasonal_signal(decimation_factor: int = 2,
+                        moving_average_win_size:
+                        Optional[int] = None) -> pd.Series | np.ndarray:
+    """
+    Loads and processes pseudo-periodic motor current signal.
+
+    :param decimation_factor: Signal downsampling factor, defaults to 2. For
+    no downsampling set 'decimation_factor=1'
+    :type decimation_factor: int
+    :param moving_average_win_size: Moving average filter window size, defaults
+    to None in which case the signal is not filtered at all.
+    :type moving_average_win_size: int, optional
+    :return: Processed pseudo-periodic signal
+    :rtype: pd.Series if 'moving_average_window_size=0' else np.ndarray
+    """
+
+    with open("data/motor_current_signals.pkl", "rb") as file:
+        data = pickle.load(file)
+
+    start = pd.to_datetime("2021-05-29 16:32")
+    end = pd.to_datetime("2021-05-29 16:40")
+    signal = data["[1017:3]"][start:end]
+
+    if decimation_factor > 1:
+        signal = decimate(signal, decimation_factor)
+
+    if moving_average_win_size:
+        signal = uniform_filter1d(signal, size=moving_average_win_size)
+
+    return signal
